4a5ec67c3889c94df6be584c3f3a6d515eba958f
birukyemane <bir_yem@yahoo.com>
2018-10-16 02:02:02 -0700

    inital commit

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React, { Component }  from 'react';
+
+class Header extends React.Component{
+    render(){
+        return <header><h1>The top ic is react</h1></header>
+    }
+}
+
+export default Header;
