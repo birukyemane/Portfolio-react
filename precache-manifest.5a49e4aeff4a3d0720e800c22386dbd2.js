d7c4c8eccc7f1dee40cb922707ae51dfec64110d
birukyemane <bir_yem@yahoo.com>
2018-10-18 05:27:07 -0700

    Updates

---
+++ precache-manifest.5a49e4aeff4a3d0720e800c22386dbd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9e24e1b95fa43a3f93aa3aa8315a988",
+    "url": "/Portfolio-react/static/media/img-placeholder.a9e24e1b.png"
+  },
+  {
+    "revision": "e15941b94fb34ad1d6854b540cb95a92",
+    "url": "/Portfolio-react/static/media/Header-img.e15941b9.png"
+  },
+  {
+    "revision": "5b4e8615077c88441ed516de6a48aa89",
+    "url": "/Portfolio-react/static/media/Border-BG.5b4e8615.jpg"
+  },
+  {
+    "revision": "506d9cc1711c510f0058",
+    "url": "/Portfolio-react/static/js/runtime~main.506d9cc1.js"
+  },
+  {
+    "revision": "241ba13bc6ce27c51d33",
+    "url": "/Portfolio-react/static/js/main.241ba13b.chunk.js"
+  },
+  {
+    "revision": "c6472c041f98c5e23486",
+    "url": "/Portfolio-react/static/js/1.c6472c04.chunk.js"
+  },
+  {
+    "revision": "241ba13bc6ce27c51d33",
+    "url": "/Portfolio-react/static/css/main.94a6fe63.chunk.css"
+  },
+  {
+    "revision": "aea3537269c3bea9bd8a64226942dc05",
+    "url": "/Portfolio-react/index.html"
+  }
+];
